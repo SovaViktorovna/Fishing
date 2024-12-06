98c288d2677d179aa3d1d389da53e2050120b886
Viktoria Demchenko <sovaviktorovna430@gmail.com>
2024-12-06 06:52:26 +0400

    Initial Commit

---
+++ FishingTests/FishingTests.swift
@@ -0,0 +1,17 @@
+//
+//  FishingTests.swift
+//  FishingTests
+//
+//  Created by Виктория Демченко on 06.12.24.
+//
+
+import Testing
+@testable import Fishing
+
+struct FishingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
